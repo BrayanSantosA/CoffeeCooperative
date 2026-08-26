2b904b9fbe479f382db4bd113161b4e4979ab9ca
Brayan Santos <bdsantosa@udistrital.edu.co>
2026-08-26 09:28:57 -0500

    fix: Change parameters

--- src/Observers/Observer.java
+++ src/Observers/Observer.java
@@ -1,7 +1,6 @@
 package observers;
-import subject.Subject;
 
 public interface Observer {
 
-	public void update(String Variety, String TasttingNotes, String NameProductor, String Origin, int Hight, int AvailableCuantity);
+	public void update(String variety, String tastingNotes, String nameProducer, String origin, int height, int availableQuantity);
 }
