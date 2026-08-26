cce6a119bd51e5fbe3e0a10e2636f2e23cf1e6c3
Brayan Santos <bdsantosa@udistrital.edu.co>
2026-08-26 09:32:38 -0500

    Add: Imports class and pribate atributes

--- src/data/CoffeData.java
+++ src/data/CoffeData.java
@@ -1,8 +1,18 @@
 package data;
-import subjects.Subject;
+import subject.Subject;
+import observers.Observer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoffeData implements Subject{
+
     private List<Observer> observers = new ArrayList<Observer>();
+	private String variety;
+	private String tastingNotes;
+	private String nameProducer;
+	private String origin;
+	private int height;
+	private int availableQuantity;
 
     public void registerObserver(Observer o) {
 		System.out.println("Suscribete para recibir notificaciones");
@@ -15,11 +25,13 @@ public class CoffeData implements Subject{
 	}
 
 	public void notifyObservers() {
-		System.out.println("\n   Hay una nueva notificación del clima  ");
+		System.out.println("\n   Hay una nueva notificación de un lote de cafe");
 		for (Observer observer : observers) {
 			System.out.print("  ");
-			observer.update(temperature, humidity, pressure);
+			observer.update(variety, tastingNotes, nameProducer, origin, height, availableQuantity);
 		}
 	}
-    
+
+
+
 }
